63da535aa1f0c9f2e78ab4eb0596c0cf0f708192
ajsantanav <adriansantana.v@gmail.com>
2025-02-03 11:09:06 -0500

    getClasses function working properly with interpolation

--- src/endpoints/getDndInfo.js
+++ src/endpoints/getDndInfo.js
@@ -1,12 +1,21 @@
-const API_URL = "https://www.dnd5eapi.co/api/";
+const API_URL = "https://www.dnd5eapi.co/api";
 
 const getClasses = async () => {
-    const url = new URL(`${API_URL}classes`);
+    const url = new URL(`${API_URL}/classes`);
     console.log(url)
     try {
         const response = await fetch(url);
         const classes = await response.json();
         console.log(classes)
+
+        const classInfo = await Promise.all(
+            classes.results.map(async (index) => {
+                console.log(index.index)
+                const classResponse = await fetch(`${url}/${index.index}`);
+                const classData = await classResponse.json();
+                
+            })
+        )
     }
     catch(err)
     {
