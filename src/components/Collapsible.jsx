9c2d86b45df555bd379c5949e34940bb86b1d809
ajsantanav <adriansantana.v@gmail.com>
2025-02-04 13:08:03 -0500

    working collapsable landing, next log in

--- src/components/Collapsible.jsx
+++ src/components/Collapsible.jsx
@@ -4,12 +4,20 @@ import React, {useState, useRef } from 'react'
 function Collapsible(props) {
 
   const [isOpen, setIsOpen] = useState(false);
-  const scrollHeight = document.getElementById("parent").scrollHeight
-
+  const parentRef = useRef(null);
+  
   return (
     <div className='collapsible'>
         <button className='toggle-collapse' onClick={()=> setIsOpen(!isOpen)}>{props.label}</button>
-        <div className='collapseContent-parent'>
+        <div 
+            className='collapseContent-parent' 
+            ref={parentRef}
+            style={isOpen ? 
+                {height: parentRef.current.scrollHeight + "px", } :
+                {
+                    height: "0px"
+                }} 
+            >
             <div className='collapseContent'>
                 {props.children}
             </div>
