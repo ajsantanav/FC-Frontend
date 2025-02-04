dcb351bf81948fb4fb2bc7a692c732f276f729eb
ajsantanav <adriansantana.v@gmail.com>
2025-02-04 12:19:02 -0500

    collapsebale wip

--- src/components/Collapsible.jsx
+++ src/components/Collapsible.jsx
@@ -1,14 +1,19 @@
-import React, {useState} from 'react'
+import React, {useState, useRef } from 'react'
 
 
 function Collapsible(props) {
 
   const [isOpen, setIsOpen] = useState(false);
+  const scrollHeight = document.getElementById("parent").scrollHeight
 
   return (
     <div className='collapsible'>
-        <button className='toggle' onClick={()=> setIsOpen(!isOpen)}>{props.label}</button>
-        {isOpen && <div className='content'>{props.children}</div>}
+        <button className='toggle-collapse' onClick={()=> setIsOpen(!isOpen)}>{props.label}</button>
+        <div className='collapseContent-parent'>
+            <div className='collapseContent'>
+                {props.children}
+            </div>
+        </div>
     </div>
   )
 }
