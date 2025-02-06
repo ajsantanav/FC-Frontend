3455358649741e01f7cd403436109b8efa8e9877
ajsantanav <adriansantana.v@gmail.com>
2025-02-05 21:38:03 -0500

    cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import React, { useState } from "react";
 import Landing from "./pages/Landing";
 import AuthForm from "./components/AuthForm";
 import Navbar from "./components/Navbar";
-import CharacterForm from "./components/CharacterForm";
 import Profile from "./pages/Profile";
+import CharacterForm from "./components/CharacterForm";
 
 function App() {
 
   return (
     <>
     <Navbar/>
+    <Profile/>
     {/* <CharacterForm/> */}
-    {/* <AuthForm /> */}
-    {/* <Landing /> */}
+    {/* <AuthForm />
+    <Landing /> */}
     </>
   );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
 
 function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <div className="profileContainer"> 
+        <div className="account-details">
+            <span>Name: Adrian Santana </span>
+            <span>Email:  juniphos@email.com</span>
+        </div>
+        <div className="char-cards-container">
+            <div className="char-card">
+                <div className="char-info">
+                    <div className="left">
+                        <span>Name:</span>
+                        <span>Race:</span>
+                    </div>
+                    <div className="right">
+                        <span>Class:</span>
+                        <span>Level:</span>
+                    </div>
+                </div>
+                <div className="stats">
+                    <ul>
+                        <li>STR: 20</li>
+                        <li>DEX: 20</li>
+                        <li>CON: 20</li>
+                        <li>INT: 20</li>
+                        <li>WIS: 20</li>
+                        <li>CHA: 20</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
